e4f7cbcc171b6fded64a9f4c64d17de73357e8dd
Sebastian Caceres <joscaceresr@correo.udistrital.edu.co>
2023-04-28 12:45:50 -0500

    Ver1

---
+++ tarea_1.py
@@ -0,0 +1,13 @@
+def CalProm(n1:float, n2:float, n3:float)->float:
+    if n1>n2:
+        if n2>n3:
+            Promedi = (n1+n2)/2
+        else:
+            Promedi = (n1+n3)/2
+    else:
+        if n1>n3:
+            Promedi = (n2+n1)/2
+        else:
+            Promedi = (n2+n3)/2
+    return Promedi
+
